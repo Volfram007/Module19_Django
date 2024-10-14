ccc04c3b7371040c5e1fe536a125e2db986cbf48
Volfram007 <60177919+Volfram007@users.noreply.github.com>
2024-10-15 00:33:01 +0300

    Задача "Настраиваем СУБД postgre в django"

--- Django/settings.py
+++ Django/settings.py
@@ -71,10 +71,21 @@ WSGI_APPLICATION = 'Django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db24',
+        'USER': 'postgres',
+        'PASSWORD': 'q135',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
